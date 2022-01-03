3151d3ab42f731db9da6c5fd82edc3ed765cdb3d
Anderson Soares <anderson.ceu@gmail.com>
2022-01-03 10:30:31 -0400

    adição de assets

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router } from "./router";
 
+import "./styles/global.scss";
+
 ReactDOM.render(
   <React.StrictMode>
     <Router />
